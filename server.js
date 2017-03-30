d7de58760021645fb0825f7581d5f60c17632225
Rory MacDonald <rorymacdonald6@hotmail.com>
2017-03-30 15:37:44 +0100

    functional local

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(express.static('client/build'));
 
 
 var server = app.listen(5000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = process.env.YOUR_HOST || '0.0.0.0';
+  var port = process.env.YOUR_PORT || process.env.PORT || 5000;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+// port: process.env.PORT || 3000 
